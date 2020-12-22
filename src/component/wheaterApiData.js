af4a266a74ac87d10e57584fa1fb58b257de7873
Sumit Singh <52070590+singh-sumit@users.noreply.github.com>
2020-12-22 19:54:47 +0545

    Cors issue updated for heroku

--- src/component/wheaterApiData.js
+++ src/component/wheaterApiData.js
@@ -4,6 +4,7 @@ import {CONFIG} from './config'
 export default class WheatherAPILoader{
     static async getCurrentWheater(city){
         let url=CONFIG.API_URL.replace('city name',city);
-        return await axios.get(url+CONFIG.API_KEY);
+        let cors="https://cors-anywhere.herokuapp.com/";
+        return await axios.get(cors+url+CONFIG.API_KEY);
     }
 }
