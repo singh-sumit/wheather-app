5e8c470738ad1ddd2a20a4fb6d5aa5448aed6783
Sumit Singh <52070590+singh-sumit@users.noreply.github.com>
2021-06-12 22:47:15 +0545

    Changed url to main website link

--- src/component/wheaterApiData.js
+++ src/component/wheaterApiData.js
@@ -4,7 +4,7 @@ import {CONFIG} from './config'
 export default class WheatherAPILoader{
     static async getCurrentWheater(city){
         let url=CONFIG.API_URL.replace('city name',city);
-        let cors="https://cors-anywhere.herokuapp.com/";
+        let cors="https://my-wheater-app.herokuapp.com/";
         return await axios.get(cors+url+CONFIG.API_KEY);
     }
 }
